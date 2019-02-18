8884cadf23c4ce3d0c8102278fe640bcd44e91ff
akshataw <wagheakshu@gmail.com>
2019-02-18 10:36:18 +0530

    Update config file

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import CreateContact from './CreateContact';
+import ContactList from './ContactList';
+import { shallow } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('MyComponent', () => {
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<App debug />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render correctly with no props', () => {
+  const component = shallow(<App />);
+  expect(component).toMatchSnapshot();
+});
 });
